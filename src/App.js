f6c214704e1936e3a3cd3d6fb8fc55a92c2f875a
Chris Perez <chrisnaperez@gmail.com>
2023-06-14 20:26:52 -0700

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 const App = () => {
-    return <div>
-      Whopper
-    </div>
+    return (
+    <>
+    <nav className='navBar'>
+                <Link to="/">Home</Link> |
+                <Link to="/routines">Routines</Link> |
+                <Link to="/profile">Profile</Link> |
+                <Link to="/account/login">Account</Link>
+            </nav>
+    </> 
+    )
   }
 
   export default App; 
